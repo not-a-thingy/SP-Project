6fca7b4ec40447ddb3133c9e904b9c1293b101f3
not-a-thingy <76759140+not-a-thingy@users.noreply.github.com>
2025-07-24 17:32:27 +0800

    fork upload

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h6><b>Web Programming Project</b></h6>
+          <h6>Presented by Group 3</h6>
+          <ul>
+            <li><a href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</footer>
---
+++ includes/search.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="searchform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Search a product</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="login_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" placeholder="Email address*">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password*">
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="remember">
+
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="login" value="Login" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
